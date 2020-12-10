333c4a062a8c6e88b3f795404cf4e5e71a603166
Thomas Ribeiro <thomashenrique2@gmail.com>
2020-12-10 11:16:57 -0300

    feat: criado cadastro de usuarios e criptografia de senha

--- backend/src/models/Appointment.ts
+++ backend/src/models/Appointment.ts
@@ -1,4 +1,13 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import User from './User';
 
 @Entity('appointments')
 class Appointment {
@@ -6,10 +15,20 @@ class Appointment {
   id: string;
 
   @Column()
-  provider: string;
+  provider_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'provider_id' })
+  provider: User;
 
   @Column('timestamp with time zone')
   date: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
 }
 
 export default Appointment;
---
+++ backend/src/routes/user.routes.ts
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import CreateUserService from '../service/CreateUserService';
+
+const usersRouter = Router();
+
+usersRouter.post('/', async (request, response) => {
+  try {
+    const { name, email, password } = request.body;
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({ name, email, password });
+
+    const userWithoutPassword = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
+
+    return response.json(userWithoutPassword);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default usersRouter;
