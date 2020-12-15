be9ec6ceddcabb27b7bb01f613f5ae659ddd6847
Thomas Ribeiro <thomashenrique2@gmail.com>
2020-12-14 23:11:09 -0300

    feat: service de autenticaçao e middleware implementados

---
+++ backend/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '9dfbc82ae767ea486575ea272817505c',
+    expiresIn: '1d',
+  },
+};
---
+++ backend/src/service/AuthenticateUserService.ts
@@ -0,0 +1,46 @@
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import { getRepository } from 'typeorm';
+import User from '../models/User';
+import authConfig from '../config/auth';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: User;
+  token: string;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error('Incorrect email/password combination');
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new Error('Incorrect email/password combination');
+    }
+
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
+      subject: user.id,
+      expiresIn,
+    });
+
+    return {
+      user,
+      token,
+    };
+  }
+}
+export default AuthenticateUserService;
